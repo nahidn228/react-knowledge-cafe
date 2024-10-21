69813bcb4c46ed634805c084669719096a4a421a
Nahid Hasan <nahidn228@gmail.com>
2024-10-21 19:36:04 +0600

    data load and set in use state done in Blog

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import Blog from "./Blog/Blog";
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("blogs.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+
+  return (
+    <div>
+      {blogs.map((blog) => (
+        <Blog blog={blog}></Blog>
+      ))}
+    </div>
+  );
+};
+
+export default Blogs;
