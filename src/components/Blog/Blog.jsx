5bf19e28f9dda712ff94c004ce89a47945e5b2bb
Nahid Hasan <nahidn228@gmail.com>
2024-10-21 20:53:36 +0600

    deploy or display blog data & mapping hashTag for add space between hastags

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,48 @@
 import PropTypes from "prop-types";
 const Blog = ({ blog }) => {
   console.log(blog);
-  return <div></div>;
+  const {
+    title,
+    cover,
+    author,
+    author_img,
+    posted_date,
+    reading_time,
+    hashTags,
+  } = blog;
+  return (
+    <div className="my-20 space-y-4">
+      <img
+        className="rounded-xl w-fit "
+        src={cover}
+        alt={`cover picture for ${title}`}
+        srcSet=""
+      />
+      <div className="flex justify-between items-center">
+        <div className="flex justify-center items-center gap-6">
+          <img className="rounded-full w-14" src={author_img} alt="" />
+          <div>
+            <h4 className="text-lg font-semibold"> {author} </h4>
+            <p className="text-gray-500 font-medium text-base">
+              {" "}
+              {posted_date}
+            </p>
+          </div>
+        </div>
+        <div>
+          <p className="text-gray-500 font-medium text-base">
+            {reading_time} min read
+          </p>
+        </div>
+      </div>
+      <h2 className="text-2xl font-semibold">Title : {title}</h2>
+      <p className="text-gray-500 font-medium text-base">
+        {hashTags.map((hash, idx) => (
+          <span key={idx}> {hash}</span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -12,6 +13,9 @@ const Blogs = () => {
   return (
     <div className="md: w-2/3">
       <h2 className="text-2xl">Blogs: {blogs.length} </h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog}></Blog>
+      ))}
     </div>
   );
 };
